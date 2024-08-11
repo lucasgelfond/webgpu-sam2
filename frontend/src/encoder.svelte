4b873d91dc41a67d63e2295ef64fdb8a76f5ff1f
Lucas Gelfond <lucas_gelfond@brown.edu>
2024-08-11 14:00:27 -0700

    svelte dropzone

---
+++ frontend/src/encoder.svelte
@@ -0,0 +1,33 @@
+<script>
+    import FileDropzone from "./file-dropzone.svelte";
+    import { sourceImage } from "$lib/source-image";
+    import { onMount } from 'svelte';
+
+
+
+</script>
+
+<div class="container">
+    {#if $sourceImage === null}
+        <FileDropzone />
+    {:else}
+        <img src={$sourceImage} alt="Uploaded image" style="width: 1024px; height: 1024px;" />
+    {/if}
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        padding: 20px;
+        font-family: 'UniversLTStd', sans-serif;
+    }
+
+    h1, p {
+        margin: 0;
+    }
+
+    p {
+        margin-top: 20px;
+    }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     import { onMount } from 'svelte';
+    import Encoder from '../encoder.svelte';
+    import { sourceImage } from '$lib/source-image';
 
     onMount(() => {
         const font = new FontFace('UniversLTStd', 'url(/fonts/UniversLTStd.woff)');
@@ -7,12 +9,17 @@
             document.fonts.add(font);
         });
     });
+
+
 </script>
 
 <div class="container">
     <h1>Segment Anything 2, in WebGPU</h1>
     <!-- TK - send blog post before shipping!-->
     <p>Built by <a href="http://lucasgelfond.online">Lucas Gelfond</a>. You can read my writeup <a href="https://lucasgelfond.online/portfolio/webgpu-sam2">here</a> or see the source code <a href="https://github.com/lucasgelfond/webgpu-sam2">here</a>.</p>
+    <p>Count ${JSON.stringify($sourceImage)}</p>
+    <Encoder/>
+
 </div>
 
 <style>
@@ -30,12 +37,8 @@
         font-family: 'UniversLTStd', sans-serif;
     }
 
-    h1, h2, p {
+    h1, p {
         margin: 0;
-    }
-
-    h2 {
-        margin-top: 10px;
     }
 
     p {
