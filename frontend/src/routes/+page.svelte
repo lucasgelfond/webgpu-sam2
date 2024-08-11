816e1efb33828dc9062aecc8f3be3492a8b4d46a
Lucas Gelfond <lucas_gelfond@brown.edu>
2024-08-11 12:25:58 -0700

    hello world

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        const font = new FontFace('UniversLTStd', 'url(/fonts/UniversLTStd.woff)');
+        font.load().then(() => {
+            document.fonts.add(font);
+        });
+    });
+</script>
+
+<div class="container">
+    <h1>Segment Anything 2, in WebGPU</h1>
+    <!-- TK - send blog post before shipping!-->
+    <p>Built by <a href="http://lucasgelfond.online">Lucas Gelfond</a>. You can read my writeup <a href="https://lucasgelfond.online/portfolio/webgpu-sam2">here</a> or see the source code <a href="https://github.com/lucasgelfond/webgpu-sam2">here</a>.</p>
+</div>
+
+<style>
+    @font-face {
+        font-family: 'UniversLTStd';
+        src: url('/fonts/UniversLTStd.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        padding: 100px;
+        font-family: 'UniversLTStd', sans-serif;
+    }
+
+    h1, h2, p {
+        margin: 0;
+    }
+
+    h2 {
+        margin-top: 10px;
+    }
+
+    p {
+        margin-top: 20px;
+    }
+</style>
