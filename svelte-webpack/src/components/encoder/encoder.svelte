14a81e012d7712c3828f62febfc66fe6b868af39
Lucas Gelfond <lucas_gelfond@brown.edu>
2024-08-12 16:52:22 -0700

    demo image

--- svelte-webpack/src/components/encoder/encoder.svelte
+++ svelte-webpack/src/components/encoder/encoder.svelte
@@ -45,6 +45,14 @@
     fileReader.readAsDataURL(file);
   }
 
+  async function handleDemoImage() {
+    const response = await fetch('https://news.cgtn.com/news/78637a4e3251444d7759444f3567444d31557a4e31457a6333566d54/img/efd368233637445391bc04444d92249e/efd368233637445391bc04444d92249e.jpg');
+    const blob = await response.blob();
+    const file = new File([blob], 'demo-image.jpg', { type: 'image/jpeg' });
+    droppedFile = file;
+    handleFile(file);
+  }
+
   onMount(() => {
     const font = new FontFace('UniversLTStd', 'url(/public/fonts/UniversLTStd.woff)');
     font.load().then(() => {
@@ -57,6 +65,11 @@
 </script>
 
 <div class="container">
+  {#if !droppedFile}
+    <div class="demo-image">
+      <span on:click={handleDemoImage} on:keydown={(e) => e.key === 'Enter' && handleDemoImage()} role="button" tabindex="0">or click here to start with a demo image</span>
+    </div>
+  {/if}
   <div class="model-selection">
     <label for="modelSize">Model Size:</label>
     <select id="modelSize" bind:value={$modelSize} disabled={!!droppedFile}>
@@ -131,5 +144,16 @@
   .model-selection select {
     border-radius: 5px;
     padding: 5px;
+  }
+  .demo-image {
+    width: 100%;
+    text-align: left;
+    margin-bottom: 10px;
+    cursor: pointer;
+    color: #0066cc;
+    text-decoration: underline;
+  }
+  .demo-image:hover {
+    color: #004080;
   }
 </style>
