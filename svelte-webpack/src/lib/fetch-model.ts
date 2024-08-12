601c526973b9e2b73e3d20c1663c52dde9f8a9e2
Lucas Gelfond <lucas_gelfond@brown.edu>
2024-08-12 11:57:36 -0700

    v1 model caching works

--- svelte-webpack/src/lib/fetch-model.ts
+++ svelte-webpack/src/lib/fetch-model.ts
@@ -1,6 +1,41 @@
 import { currentStatus } from './current-status';
 
 async function fetchModel(modelURL: string, modelName: string) {
+  currentStatus.set(`Getting ${modelName} model...`);
+  let cachedModel;
+
+  try {
+    const root = await navigator.storage.getDirectory();
+    const modelFile = await root.getFileHandle(modelName, { create: false });
+    cachedModel = await modelFile.getFile();
+    console.log(`Found cached ${modelName} model`);
+  } catch (error) {
+    console.log(`No cached ${modelName} model. Error: ${error.message}`);
+  }
+
+  if (cachedModel) {
+    console.log(`Using cached model for ${modelName}`);
+    return cachedModel.arrayBuffer();
+  } else {
+    console.log(`Fetching model for ${modelName} from URL: ${modelURL}`);
+    const fetchedModel = await innerFetchModel(modelURL, modelName);
+
+    try {
+      const root = await navigator.storage.getDirectory();
+      const modelFile = await root.getFileHandle(modelName, { create: true });
+      const writable = await modelFile.createWritable();
+      await writable.write(fetchedModel);
+      await writable.close();
+      console.log(`Cached model for URL: ${modelURL}`);
+    } catch (error) {
+      console.error(`Failed to cache model for URL: ${modelURL}. Error: ${error.message}`);
+    }
+
+    return fetchedModel;
+  }
+}
+
+async function innerFetchModel(modelURL: string, modelName: string) {
   currentStatus.set(`Fetching ${modelName} model..."`);
   const response = await fetch(modelURL, {
     method: 'GET',
